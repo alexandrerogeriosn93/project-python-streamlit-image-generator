0b1ffc552f6883ba51ab69f3abbc3fafbdc36e3d
Alexandre Rogério <alexandrerogeriosn93@gmail.com>
2024-12-02 19:17:50 -0300

    feat: create function to generate image

--- main.py
+++ main.py
@@ -7,3 +7,11 @@ from PIL import Image
 
 load_dotenv()
 openai.api_key = os.getenv("API_KEY")
+
+
+def generate_image(description):
+    image_response = openai.Image.create(prompt=description, n=1, size="512x512")
+    image_url = image_response["data"][0]["url"]
+    urllib.request.urlretrieve(image_url, "image.png")
+    image = Image.open("image.png")
+    return image
