dafe83caa0144ef23266c8e9fd2bf8edded657f2
Alexandre Rogério <alexandrerogeriosn93@gmail.com>
2024-12-02 19:17:04 -0300

    feat: import modules and load virtual env

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print("Hello World")
+import os
+import openai
+import streamlit as st
+import urllib.request
+from dotenv import load_dotenv
+from PIL import Image
+
+load_dotenv()
+openai.api_key = os.getenv("API_KEY")
